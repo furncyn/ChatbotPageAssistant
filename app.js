ee0f6c768e42cb874caeac8a74b09261b0a0519c
Furn Techalertumpai <furncyn@fb.com>
2020-11-30 15:11:17 +0800

    Add page state schema

--- app.js
+++ app.js
@@ -31,6 +31,55 @@ const
   body_parser = require('body-parser'),
   app = express().use(body_parser.json()); // creates express http server
 
+const STATE = {
+  /** Module 1 */
+  1: "Profile photo",
+  /**
+   * A: user uploads profile photo
+   * B: suggestion about profile photo
+   */
+  2: "Cover photo",
+  /**
+   * A: user uploads cover photo
+   * B: suggestion about cover photo
+   */
+  3: "Page category",
+  4: "Page location",
+
+  5: "Opening hours",
+  /**
+   * A: 4 selections (No hours, Always open, Permanently closed, Selected hours)
+   * B: specify Selected hours
+   */
+  6: "Contact info", // to be finalized
+
+  /** Module 2 */
+  7: "Appointment booking",
+  // only show if user wants to set up Appointment booking
+  8: "Appointment flow",
+  /**
+   * A: select days and times for appointment
+   * B: appointment approval (bool)
+   * C: double-booking (bool)
+   * D: sync to google calendar (bool)
+   */
+  9: "Service flow",
+  /**
+   * A: service name
+   * B: price
+   * C: description (optional)
+   * D: duration
+   * E: include an image
+  */
+  10: "Link WA",
+  /**
+   * A: ask for phone number
+   * B: ask for verification code
+   * C: add contact button on page
+   */
+  11: "Automated reply",
+}
+
 // Sets server port and logs message on success
 app.listen(1337, () => console.log('webhook is listening on port 1337'));
 
@@ -123,8 +172,8 @@ function handleMessage(sender_psid, received_message) {
         "quick_replies": [
           {
             "content_type": "text",
-            "title": "Health/Beauty",
-            "payload": "health",
+            "title": "Beauty service",
+            "payload": "beauty",
           }, {
             "content_type": "text",
             "title": "Dining",
