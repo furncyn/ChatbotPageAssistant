ed7c37060e781f551faa29fd2ea215a9b00cb549
Tu Anqi <anqitu@outlook.com>
2020-12-01 23:06:43 +0800

    Put back message for webhook

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.post('/' + PREFIX + '/webhook', (req, res) => {
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
       if (webhook_event.message) {
-        handleMessage(sender_psid, webhook_event);
+        handleMessage(sender_psid, webhook_event.message);
       } else if (webhook_event.postback) {
         handlePostback(sender_psid, webhook_event.postback);
       }
@@ -111,8 +111,7 @@ app.get('/' + PREFIX + '/webhook', (req, res) => {
 common.sendResponse('4995283947150222', RESPONSES.GET_STARTED);
 db.setUserState('4995283947150222', 1, 'A');
 
-function handleMessage(sender_psid, webhook_event) {
-  const received_message = webhook_event.message;
+function handleMessage(sender_psid, received_message) {
   let response;
   try {
     const debugReponse = getDebugReponse(received_message);
