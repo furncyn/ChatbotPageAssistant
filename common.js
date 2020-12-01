b54a981f228d29733b14e74edb13b6e0e2edb136
Tu Anqi <anqitu@outlook.com>
2020-12-01 21:05:07 +0800

    Refactor sendResponse

--- common.js
+++ common.js
@@ -1,40 +1,38 @@
 const rp = require('request-promise');
-const request = require('request');
 const sequential = require('promise-sequential');
 const PAGE_ACCESS_TOKEN = process.env.PAGE_TOKEN;
 
-function sendResponse(sender_psid, response) {
+async function sendResponse(sender_psid, response) {
   if (Array.isArray(response)) {
-    for (r of response) {
-      callSendAPI(sender_psid, r);
-    }
+    const async_actions = response.map(r => callSendAPI(sender_psid, r));
+    const res = await sequential(async_actions);
+    console.log("all done");
+    console.log(res);
   } else {
-    callSendAPI(sender_psid, response)
+    const res = await callSendAPI(sender_psid, response)
+    console.log("all done");
+    console.log(res);
   }
 }
 
 function callSendAPI(sender_psid, response) {
   // Construct the message body
-  let request_body = {
+  const request_body = {
     "recipient": {
       "id": sender_psid
     },
     "message": response
-  }
+  };
 
-  // Send the HTTP request to the Messenger Platform
-  request({
+  const options = {
     "uri": "https://graph.facebook.com/v2.6/me/messages",
     "qs": { "access_token": PAGE_ACCESS_TOKEN },
     "method": "POST",
     "json": request_body
-  }, (err, res, body) => {
-    if (!err) {
-      console.log('message sent!')
-    } else {
-      console.error("Unable to send message:" + err);
-    }
-  });
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  return rp(options);
 }
 
 module.exports.sendResponse = sendResponse;
